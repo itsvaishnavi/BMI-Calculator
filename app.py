9d5251edd2b87af53cad8b7106cc6f9778871f8c
Vaishnavi Piyush <61691914+itsvaishnavi@users.noreply.github.com>
2022-01-16 12:08:10 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,17 @@
+import flask
+from flask import Flask, render_template, request
+
+app = flask.Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def api_all():
+	st=''
+	if request.method == "POST" and 'weight' in request.form:
+		weight = float(request.form.get('weight'))
+		height = float(request.form.get('height'))
+		bmi = weight/((height/100)**2)
+		st="Calculated BMI is "+str(round(bmi,2))
+	return render_template("temp.html", data=st)
+
+if __name__ == '__main__':
+    app.run(debug=True)
